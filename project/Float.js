2b160849f4103a994e115031a6b8958c973f8447
Naivedh <naivedh.170410107104@gmail.com>
2019-12-05 19:58:50 +0530

    Add files via upload

---
+++ project/Float.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#CAR").click(function () {
+        $("#yo").show();
+        $("#yo").fadeIn('slow');
+    });
+    $("#SOS").click(function () {
+        $("#yo").show();
+        $("#yo").fadeIn('slow');
+    });
+});
